3fb72015d2aa5aa6d18a33eeb710aaa0f67265c6
Caedmon07 <danielsmith77@btinternet.com>
2023-12-09 20:15:40 +0000

    Update StockMarket.py

--- StockMarket.py
+++ StockMarket.py
@@ -20,6 +20,25 @@ for ticker in stock_tickers:
 
 data=pd.concat(dfs, ignore_index=True)
 
-data = data[["Ticker", "Date", "Open", "High", "Low", "Close", "Adj Close", "Volume"]]
+figure = go.Figure()
 
-print(data)
+for ticker in stock_tickers:
+    stock_data = data[data['Ticker'] == ticker]
+
+figure.add_trace(go.Candlestick(x=stock_data['Date'],
+                             open=stock_data['Open'],
+                             high=stock_data['High'],
+                             low=stock_data['Low'],
+                             close=stock_data['Close'],
+                             text=stock_data['Ticker'] + '<br>Open: ' + stock_data['Open'].astype(str) + 
+                                       '<br>Close: ' + stock_data['Close'].astype(str) + 
+                                       '<br>High: ' + stock_data['High'].astype(str) + 
+                                       '<br>Low: ' + stock_data['Low'].astype(str), 
+                                       hoverinfo='text', 
+                                       name=data['Ticker']))  
+
+figure.update_layout(title = "Muliple Stock Price Analysis")
+figure.update_xaxes(title_text='Date')
+figure.update_yaxes(title_text='Stock Price (USD)')
+
+figure.show()

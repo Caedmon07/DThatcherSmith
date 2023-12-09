043a2011b18c5783113917a6101a58381e1fc8f2
Caedmon07 <danielsmith77@btinternet.com>
2023-12-09 21:42:42 +0000

    Update StockMarket.py

--- StockMarket.py
+++ StockMarket.py
@@ -1,7 +1,6 @@
 import numpy as np
 import yfinance as yf
 import pandas as pd
-import datetime
 from datetime import date, timedelta
 import plotly.graph_objects as go
 import plotly.express as px
@@ -10,35 +9,97 @@ today = date.today()
 end_date = today.strftime("%Y-%m-%d")
 start_date = (today - timedelta(days=365)).strftime("%Y-%m-%d")
 
-stock_tickers=['AAPL', 'GOOG', 'MSFT', 'AMZN']
+stock_colors={'AAPL': 'green', 'GOOG': 'blue', 'MSFT': 'orange', 'AMZN': 'red'}
 
-dfs = []
-for ticker in stock_tickers:
+dfs = {}
+for ticker, color in stock_colors.items():
     data = yf.download(ticker, start=start_date, end=end_date, progress=False).reset_index()
     data["Ticker"] = ticker
-    dfs.append(data)
+    data["Colour"] = color
+    dfs[ticker] = data
 
-data=pd.concat(dfs, ignore_index=True)
+data=pd.concat(list(dfs.values()), ignore_index=True)
 
+#Creates a Candlestick Graph
 figure = go.Figure()
 
-for ticker in stock_tickers:
-    stock_data = data[data['Ticker'] == ticker]
-
-figure.add_trace(go.Candlestick(x=stock_data['Date'],
-                             open=stock_data['Open'],
-                             high=stock_data['High'],
-                             low=stock_data['Low'],
-                             close=stock_data['Close'],
-                             text=stock_data['Ticker'] + '<br>Open: ' + stock_data['Open'].astype(str) + 
-                                       '<br>Close: ' + stock_data['Close'].astype(str) + 
-                                       '<br>High: ' + stock_data['High'].astype(str) + 
-                                       '<br>Low: ' + stock_data['Low'].astype(str), 
+for ticker in stock_colors.keys():
+    subset = data[data['Ticker'] == ticker]
+    color = stock_colors[ticker]
+    figure.add_trace(go.Candlestick(x=subset['Date'],
+                             open=subset['Open'],
+                             high=subset['High'],
+                             low=subset['Low'],
+                             close=subset['Close'],
+                             increasing_line_color=color,
+                             decreasing_line_color=color,
+                             text=subset['Ticker'] + '<br>Open: ' + subset['Open'].astype(str) + 
+                                       '<br>Close: ' + subset['Close'].astype(str) + 
+                                       '<br>High: ' + subset['High'].astype(str) + 
+                                       '<br>Low: ' + subset['Low'].astype(str), 
                                        hoverinfo='text', 
-                                       name=data['Ticker']))  
+                                       name=ticker))  
 
 figure.update_layout(title = "Muliple Stock Price Analysis")
 figure.update_xaxes(title_text='Date')
 figure.update_yaxes(title_text='Stock Price (USD)')
 
+#Creates a Bar Plot
+figure2 = go.Figure()
+for ticker in stock_colors.keys():
+    subset = data[data['Ticker'] == ticker]
+    color = stock_colors[ticker]
+    figure2.add_trace(go.Bar(x=subset['Date'], 
+                            y=subset['Close'], 
+                            marker_color=color,
+                            text=subset['Ticker'] + '<br>Close: ' + subset['Close'].astype(str),
+                            hoverinfo='text',
+                            name=ticker))
+    figure2.update_layout(title = "Muliple Stock Price Analysis (Bar Plot)")
+    figure2.update_xaxes(title_text='Date')
+    figure2.update_yaxes(title_text='Closing Price (USD)')
+
+figure3=go.Figure()
+for ticker, df in dfs.items():
+    color = stock_colors[ticker]
+    figure3.add_trace(go.Scatter(x=df['Date'],
+                             y=df['Close'],
+                             mode='lines',
+                             line=dict(color=color),
+                             text=df['Ticker'] + '<br>Close: ' + data['Close'].astype(str), 
+                             hoverinfo='text', 
+                             name=ticker))  
+
+figure3.update_xaxes(title_text='Date')
+figure3.update_yaxes(title_text="Close Price (USD)")
+figure3.update_layout(title_text='Stock Prices Line Graph')
+
+date_ranges = {
+    '1 Month': 30,
+    '3 Months': 90,
+    '6 Months': 180,
+    '1 Year': 365
+}
+
+updatemenus =[]
+
+for stock in stock_colors.keys():
+    buttons = []
+    for label, days in date_ranges.items():
+        button = dict(label=label,
+                      method='relayout',
+                      args=[{'xaxis': {'range': [today - timedelta(days=days), today]}}])
+        buttons.append(button)
+
+        updatemenus.append(dict(type='buttons',
+                                showactive=True,
+                                buttons=buttons,
+                                x=0.1,
+                                xanchor='left',
+                                y=1.15,
+                                yanchor='top'))
+figure3.update_layout(updatemenus=updatemenus)
+
 figure.show()
+figure2.show()
+figure3.show()

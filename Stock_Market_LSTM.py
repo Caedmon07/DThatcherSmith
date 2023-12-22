9eea4f0ac22fca3f11eaf37d6c66fbf35ed548a3
Caedmon07 <danielsmith77@btinternet.com>
2023-12-22 08:32:39 +0000

    Update Stock_Market_LSTM.py

--- Stock_Market_LSTM.py
+++ Stock_Market_LSTM.py
@@ -19,12 +19,14 @@ from keras.regularizers import l2
 from keras.callbacks import EarlyStopping
 from sklearn.ensemble import RandomForestRegressor
 from statsmodels.tsa.arima.model import ARIMA
+import xgboost as xgb
+import matplotlib.pyplot as plt
 
 # Global Constants
 SYMBOLS = ['GOOG', 'MSFT']
 START_DATE = '2020-01-01'
 END_DATE = str(date.today())
-NUM_DAYS = 1
+NUM_DAYS = 5
 SERVER = 'LAPTOP-B5J6KRT7'
 DATABASE = 'PortfolioProject'
 
@@ -49,15 +51,23 @@ def send_email(subject, body):
 def get_sql_credentials():
     """Get SQL Server credentials."""
     username = input("Enter your SQL Server username:")
-    try:
-        password = tkinter.simpledialog.askstring("Login", "Enter your SQL Server password:", show='*')
-    except AttributeError:
-        # Handle the case when tkinter.simpledialog is not available
-        print("Warning: tkinter.simpledialog is not available. Please proved the password directly.")
-        password = input("Enter your SQL Server password: ")
+    password = tkinter.simpledialog.askstring("Login", "Enter your SQL Server password:", show='*')
+    
     return username, password
 
-def download_stock_data(SYMBOLS, START_DATE, END_DATE, SERVER, DATABASE):
+def plot_graph(data, title, xlabel, ylabel):
+    """Plot a graph"""
+    plt.figure(figsize=(10, 6))
+    plt.plot(data, label="predictions")
+    plt.title(title)
+    plt.xlabel(xlabel)
+    plt.ylabel(ylabel)
+    plt.grid(True)
+    plt.show()
+
+    print("Data to be plotted: ", data)
+
+def download_stock_data(SYMBOLS, START_DATE, END_DATE):
     """Download stock data."""
     username, password = get_sql_credentials()
     all_data = []
@@ -74,7 +84,7 @@ def download_stock_data(SYMBOLS, START_DATE, END_DATE, SERVER, DATABASE):
 
     return all_data
 
-all_data = download_stock_data(SYMBOLS, START_DATE, END_DATE, SERVER, DATABASE)
+all_data = download_stock_data(SYMBOLS, START_DATE, END_DATE)
 
 def preprocess_data(data, scaler_x, scaler_y):
     """Preprocess stock data."""
@@ -140,6 +150,9 @@ def predictions_lstm(model, scaler_x, scaler_y, data, num_days):
 
     return future_predictions
 
+def plot_graph_LSTM(future_predictions):
+    plot_graph(future_predictions, "LSTM Future Predictions", "Days", "Stock Price")
+
 def predict_stock_random_forest(data, num_days):
     """Predict stock using a Random Forest model."""
     x = data[["Open", "High", "Low", "Volume"]]
@@ -172,6 +185,8 @@ def predict_stock_random_forest(data, num_days):
 
     future_predictions_rf = scaler_y.inverse_transform(np.array(future_predictions_rf).reshape(-1, 1))
 
+    plot_graph(future_predictions_rf, "Random Forest Future Predictions", "Days", "Stock Price")
+
     return rf_model, future_predictions_rf
 
 def predict_stock_arima(data, num_days):
@@ -183,11 +198,47 @@ def predict_stock_arima(data, num_days):
 
     future_predictions_arima = arima_results.predict(start=len(y), end=len(y) + num_days - 1, type='levels')
 
+    plot_graph(future_predictions_arima, "ARIMA Future Predictions", "Days", "Stock Price")
+
     return np.squeeze(future_predictions_arima)
 
-def blend_predictions(predictions_lstm, predictions_rf, predictions_arima):
+def predict_stock_xgboost(data, num_days):
+    """Predict stock using an XGBoost model"""
+    x = data[["Open", "High", "Low", "Volume"]]
+    y = data["Close"]
+
+    scaler_x = MinMaxScaler()
+    scaler_y = MinMaxScaler()
+
+    x = scaler_x.fit_transform(x)
+    y = scaler_y.fit_transform(y.values.reshape(-1, 1))
+
+    xtrain, xtest, ytrain, ytest = train_test_split(x, y, test_size=0.2, random_state=42)
+
+    xgboost_model = xgb.XGBRegressor(objective='reg:squarederror', n_estimators=100, random_state=42)
+    xgboost_model.fit(xtrain, ytrain.ravel())
+
+    test_predictions = xgboost_model.predict(xtest)
+    test_predictions = scaler_y.inverse_transform(test_predictions.reshape(-1, 1))
+
+    last_data_point = data.tail(1)[["Open", "High", "Low", "Volume"]].values.reshape(1, -1)
+    last_data_point = scaler_x.transform(last_data_point)
+
+    future_predictions_xgboost = []
+
+    for _ in range(num_days):
+        future_prediction = xgboost_model.predict(last_data_point.reshape(1, -1))
+        future_predictions_xgboost.append(future_prediction[0])
+
+        last_data_point = np.append(last_data_point[:, 1:], np.array(future_predictions_xgboost).reshape(-1, 1))
+    
+    plot_graph(future_predictions_xgboost, "XGBoost Future Preditions", "Days", "Stock Price")
+
+    return xgboost_model, future_predictions_xgboost
+
+def blend_predictions(future_predictions, future_predictions_rf, future_predictions_arima, future_predictions_xgboost):
     """Blend predictions from different models."""
-    blended_predictions = (predictions_lstm + predictions_rf + predictions_arima) / 3.0
+    blended_predictions = (future_predictions + future_predictions_rf + future_predictions_arima + future_predictions_xgboost) / 4.0
     return blended_predictions
 
 def plot_candlestick_chart(all_data, future_predictions, symbols, num_days):
@@ -232,7 +283,7 @@ def plot_candlestick_chart(all_data, future_predictions, symbols, num_days):
     figure.update_layout(title=f"Stock Price Analysis with Predictions", xaxis_rangeslider_visible=False)
     figure.show()
 
-def live_stock_feed(symbols, all_data, scaler_x_lstm, scaler_y_lstm, model_lstm, rf_model, scaler_y_rf, arima_results, num_days):
+def live_stock_feed(symbols, all_data, scaler_x_lstm, scaler_y_lstm, model_lstm, rf_model, scaler_y_rf, arima_results, xgboost_model, num_days):
     """Live stock feed."""
     num_live_features = 4  # Set the number of live features
     scaler_x_lstm.fit(all_data[0][["Open", "High", "Low", "Volume"]].values[:, :num_live_features])
@@ -246,37 +297,42 @@ def live_stock_feed(symbols, all_data, scaler_x_lstm, scaler_y_lstm, model_lstm,
         x_live = scaler_x_lstm.transform(x_live)
 
         print(x_live.shape)
-        # Ensure x_live has the correct shape for LSTM input
         x_live_reshaped = x_live.reshape((x_live.shape[0], num_live_features, 1))
 
         prediction_lstm = model_lstm.predict(x_live_reshaped)
         prediction_rf = rf_model.predict(x_live)
-        # Specify start and end parameters for ARIMA prediction
         prediction_arima = arima_results.predict(start=len(data), end=len(data), type='levels')
+        prediction_xgboost = xgboost_model.predict(x_live)
 
         predicted_close_lstm = scaler_y_lstm.inverse_transform(prediction_lstm.reshape(-1, 1))[0][0]
         predicted_close_rf = scaler_y_rf.inverse_transform(prediction_rf.reshape(-1, 1))[0][0]
         predicted_close_arima = prediction_arima[-1]
+        predicted_close_xgboost = scaler_y_lstm.inverse_transorm(prediction_xgboost.reshape(-1, 1))[0][0]
 
         print(f"Predicted Close (LSTM) for the next day: {predicted_close_lstm}")
         print(f"Predicted Close (RF) for the next day: {predicted_close_rf}")
         print(f"Predicted close (ARIMA) for the next day: {predicted_close_arima}")
+        print(f"Predicted close (XGBoost) for the next day: {predicted_close_xgboost}")
 
         send_email(f"Stock Prediction  ({symbols})",
                    f"Predicted Close (LSTM) for the next day: {predicted_close_lstm}\n"
                    f"Predicted Close (RF) for the next day: {predicted_close_rf}\n"
-                   f"Predicted close (ARIMA) for the next day: {predicted_close_arima}\n")
+                   f"Predicted close (ARIMA) for the next day: {predicted_close_arima}\n"
+                   f"Predicted close (XGBoost) for the next day: {predicted_close_xgboost}\n")
 
         time.sleep(86400)
 
 all_data = download_stock_data(SYMBOLS, START_DATE, END_DATE)
 scaler_x_lstm = MinMaxScaler()
 scaler_y_lstm = MinMaxScaler()
-model_lstm = build_lstm_model(input_shape=(4, 1))  # Assuming 4 features in the input
+model_lstm = build_lstm_model(input_shape=(4, 1))
 future_predictions_lstm = predictions_lstm(model_lstm, scaler_x_lstm, scaler_y_lstm, all_data[0], NUM_DAYS)
 future_predictions_rf = predict_stock_random_forest(all_data[0], NUM_DAYS)
 future_predictions_arima = predict_stock_arima(all_data[0], NUM_DAYS)
+future_predictions_xgboost = predict_stock_xgboost(all_data[0], NUM_DAYS)
 blended_predictions = blend_predictions(future_predictions_lstm, future_predictions_rf, future_predictions_arima)
+live_stock_feed(SYMBOLS, all_data, scaler_x_lstm, scaler_y_lstm, model_lstm=model_lstm, 
+                 rf_model=future_predictions_rf, arima_results=future_predictions_arima, num_days=NUM_DAYS)
 plot_candlestick_chart(all_data, [blended_predictions], SYMBOLS, NUM_DAYS)
 print("LSTM Future Predictions")
 print(future_predictions_lstm)
@@ -286,6 +342,3 @@ print("ARIMA Future Predictions")
 print(future_predictions_arima)
 print("Blended Predictions")
 print(blended_predictions)
-
-live_stock_feed(SYMBOLS, all_data, scaler_x_lstm, scaler_y_lstm, model_lstm=model_lstm, 
-                 rf_model=future_predictions_rf, arima_results=future_predictions_arima, num_days=NUM_DAYS)
